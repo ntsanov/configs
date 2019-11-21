54438d46e6c4bf3cd920a98ee3573ca94773b710
tsanov86@gmail.com <tsanov86@gmail.com>
2019-11-21 22:43:33 +0100

    fixed spelling error

--- install_vim_configs.sh
+++ install_vim_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.ntsanov-configs/.vimrc ~/.vimrc
-ln -s ~/.ntsanov-conifgs/.vim/startup ~/.vim/startup
+ln -s ~/.ntsanov-configs/.vim/startup ~/.vim/startup
 vim +PluginInstall +qall
