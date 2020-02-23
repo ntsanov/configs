c7c1d789e85590a3c735005944334ab576ddbc4a
ntsanov <tsanov86@gmail.com>
2020-02-23 14:37:25 +0200

    Removed some hard coded paths

--- install_vim_configs.sh
+++ install_vim_configs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ~/.ntsanov-configs/.vimrc ~/.vimrc
-ln -s ~/.ntsanov-configs/.vim/startup ~/.vim/startup
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ln -s ${DIR}/.vimrc ~/.vimrc
+ln -s ${DIR}/.vim/startup ~/.vim/startup
 vim +PluginInstall +qall
