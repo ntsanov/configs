4103a80fb111bb86797248919fe3c65968fc8f73
ntsanov <tsanov86@gmail.com>
2021-08-13 09:35:28 +0300

    Added dunstrc to dotfiles

--- init_configs.sh
+++ init_configs.sh
@@ -3,8 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 rm -rf ~/.config/nvim
 rm -rf ~/.config/fontconfig
+rm -rf ~/.config/dunst
 rm -rf ~/.zshrc
 
 ln -s ${DIR}/.config/nvim ~/.config/nvim
 ln -s ${DIR}/.config/fontconfig ~/.config/fontconfig
+ln -s ${DIR}/.config/dunst ~/.config/dunst
 ln -s ${DIR}/.zshrc ~/.zshrc
