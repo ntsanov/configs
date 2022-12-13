f53ff3d664cae90f165c5dd96feeec623aed2ef6
ntsanov <tsanov86@gmail.com>
2022-12-13 10:29:19 +0200

    Initial alacritty config

--- init_configs.sh
+++ init_configs.sh
@@ -5,8 +5,10 @@ rm -rf ~/.config/nvim
 rm -rf ~/.config/fontconfig
 rm -rf ~/.config/dunst
 rm -rf ~/.zshrc
+rm -rf ~/.config/alacritty
 
 ln -s ${DIR}/.config/nvim ~/.config/nvim
 ln -s ${DIR}/.config/fontconfig ~/.config/fontconfig
 ln -s ${DIR}/.config/dunst ~/.config/dunst
 ln -s ${DIR}/.zshrc ~/.zshrc
+ln -s ${DIR}/.config/alacritty ~/.config/alacritty
